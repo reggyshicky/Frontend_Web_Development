545c11979e3fd0e3fb93750a91ba8dad30912675
reggyshicky <reginahshikanda@gmail.com>
2023-07-06 16:25:49 +0300

    Draw a square

--- cs50/make.c
+++ cs50/make.c
@@ -3,7 +3,15 @@
 
 int main(void)
 {
-    printf("Hello World\n");
-    return (0);
+    const int n = 10; /*a variable that holds the value of i and j since its a square, easier to just change n at once*/
+       /* for the const, you cant change the n value elsewhere, as it will result to an error*/
+    int i;
+    int j;
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n; j++) 
+        printf("#");
+        printf("\n");
+    }
     
 }
