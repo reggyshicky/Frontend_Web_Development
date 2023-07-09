c4480cdb069b02b1581629767bb2f51c245772d7
reggyshicky <reginahshikanda@gmail.com>
2023-07-09 12:59:30 +0300

    turtle-graphics for drawing a circle

---
+++ cs50/turtle_graphics_circle.py
@@ -0,0 +1,15 @@
+import turtle as t
+import time
+
+t.title = "2nd Turtle"
+t.bgcolor("black")
+t.pensize(5)
+t.speed(1)
+t.color("blue")
+
+t.begin_fill()
+
+t.circle(100) #function that draws a circle of a 100 pixels radis
+
+t.end_fill
+time.sleep(20)
