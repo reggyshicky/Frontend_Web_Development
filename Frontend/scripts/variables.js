101b81bc97d7853d47c422cc0cf0c07f753428b1
reggyshicky <reginahshikanda@gmail.com>
2023-10-03 16:26:09 +0300

    functions format

--- Frontend/scripts/variables.js
+++ Frontend/scripts/variables.js
@@ -25,7 +25,17 @@ const functionVariable = function() {
 }
 functionVariable();
 
-const mystring = 'Javascript';
+const mystring = 'Javascript language';
 newstring = mystring.replace('v', 'b');
 console.log(newstring);
 
+upperstring = mystring.toUpperCase().split(" ");
+console.log(upperstring);
+result = upperstring.indexOf('LANGUAGE');
+console.log(result);
+// In JavaScript, the split() method is used to split a string into an array of substrings based on a specified 
+// separator. The split() method takes one or two arguments:
+// Separator (Required): The separator is a string or a regular expression that specifies where the original 
+// string should be split. This is the only required argument. The split() method splits the string into an 
+// array of substrings wherever the separator is found. If the separator is an empty string (""), the string is 
+// split into an array of individual characters.
