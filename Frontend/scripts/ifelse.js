e10996efc8355aa4e9413eda0bf1dfa9f63cbbb5
reggyshicky <reginahshikanda@gmail.com>
2023-10-07 19:29:00 +0300

    if else, template literals

--- Frontend/scripts/ifelse.js
+++ Frontend/scripts/ifelse.js
@@ -7,3 +7,9 @@ function greet (name, surname) {
     }
 }
 console.log(greet('reggy', 'shicky'));
+
+//template literals
+var x = 5;
+var y = 10;
+var sum = `The sum of ${x} and ${y} is ${x + y}.`;
+console.log(sum); // Output: "The sum of 5 and 10 is 15."
