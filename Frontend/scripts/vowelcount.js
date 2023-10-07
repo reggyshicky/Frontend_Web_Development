405099cf98d65d500433d1cf5ea80b393c38d0a3
reggyshicky <reginahshikanda@gmail.com>
2023-10-07 18:50:24 +0300

    used switch case to counts vowels in a string/alternative

--- Frontend/scripts/vowelcount.js
+++ Frontend/scripts/vowelcount.js
@@ -10,3 +10,34 @@ function getCount(str) {
     return count
 }
 console.log(getCount('abiou'))
+
+
+//alternative
+function getCount1(str) {
+    let vowelsCount = 0;
+
+    const arr = str.split(""); //splits the string into an array of characters
+
+    for (i = 0; i < arr.length; i++) {
+        switch(arr[i]) {
+            case 'a':
+                vowelsCount++;
+                break;
+            case 'e':
+                vowelsCount++;
+                break;
+            case 'i':
+                vowelsCount++;
+                break;
+            case 'o':
+                vowelsCount;
+                break;
+            case 'u':
+                vowelsCount++;
+                break;
+        }
+    }
+    return vowelsCount;
+
+}
+console.log(getCount1("aeioufssjdkxvnmedfb"))
