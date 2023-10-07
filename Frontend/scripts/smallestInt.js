e776b51a557e5c52c0ef4f361a7f431ddbf65c70
reggyshicky <reginahshikanda@gmail.com>
2023-10-07 20:41:45 +0300

    max and min integer of an array using Math.max and Math.min, also destructing the array, using three dots ...

---
+++ Frontend/scripts/smallestInt.js
@@ -0,0 +1,19 @@
+//Given an array of integers your solution should find the smallest integer.
+//Find the minimum and maximum integers in an array
+function MaxMinInteger (args) {
+    let max_int = Math.max(...args);
+    let min_int = Math.min(...args);
+    return `${min_int} & ${max_int}`; 
+}
+
+console.log(MaxMinInteger([5, 10, 1, 0, 90]))
+//The destructuring assignment syntax is a JavaScript expression that makes it possible to 
+//extract data from arrays or objects into distinct variables.
+// const nums = [1, 2, 3]
+// Math.min(...nums)    // 1
+// Math.max(...nums)    // 3
+// … in front of an array will convert array to distinct variables and send them to the function, 
+// which is equivalent to
+
+// Math.min(1, 2, 3)
+// Math.max(1, 2, 3)
