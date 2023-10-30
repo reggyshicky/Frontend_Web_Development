5ae019acbbb77b20ff57cdac3086b08442376e45
reggyshicky <reginahshikanda@gmail.com>
2023-10-30 19:35:31 +0300

    use of string.repeat(count)

--- Frontend/scripts/repeatstr.js
+++ Frontend/scripts/repeatstr.js
@@ -8,3 +8,9 @@ function repeatStr(numberOfRepeats, str) {
     return starterString;
 }
 console.log(repeatStr(4, "Reginah"));
+
+//alternative
+//simply do this
+//Syntax : string.repeat(count)
+str.repeat(numberOfRepeats);
+"https://www.w3schools.com/jsref/jsref_repeat.asp#:~:text=The%20repeat()%20method%20returns,not%20change%20the%20original%20string."
