a6884bbebd0a23fa3979100793ed9dd73e9c94d5
reggyshicky <reginahshikanda@gmail.com>
2023-10-30 19:20:42 +0300

    repeated strings

--- Frontend/scripts/repeatstr.js
+++ Frontend/scripts/repeatstr.js
@@ -1,8 +1,10 @@
 //Write a function called which repears the given string exactly count times
-// n is the count, s is the string
-function repeatStr(n, s) {
-    for (i = 0; i < n; i++) {
-        console.log(s)
+// n is the count, s is the string i.e repeatStr(3, j) 'jjjjj'
+function repeatStr(numberOfRepeats, str) {
+    let starterString = '';
+    for (let i = 0; i < numberOfRepeats; i++) {
+        starterString = starterString + str
     }
+    return starterString;
 }
-repeatStr(4, "Reginah");
+console.log(repeatStr(4, "Reginah"));
