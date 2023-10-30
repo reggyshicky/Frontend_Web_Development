829ffcf88ce4aba59a1b57f3490488f7e9f2fe2c
reggyshicky <reginahshikanda@gmail.com>
2023-10-30 21:13:43 +0300

    split a string into an array

---
+++ Frontend/scripts/split_string.js
@@ -0,0 +1,6 @@
+//write a function to split a string and convert it into an array of words.
+function stringToArray(string) {
+    let newstring = string.split(" ")
+    return newstring;
+}
+console.log(stringToArray("Hello how are you"));
