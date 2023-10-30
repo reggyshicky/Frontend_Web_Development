08c923866c0a492c68ddc78071177142257fbbec
reggyshicky <reginahshikanda@gmail.com>
2023-10-30 19:02:10 +0300

    basic calculator in js

---
+++ Frontend/scripts/basic_calculator.js
@@ -0,0 +1,20 @@
+//your task is to create a function that does four basic mathematical operations
+//The function should take 3 arguments - operation(string/char). value1(number), value2(number)
+//The function should return result of the numbers after applying the chosen operations
+
+function basicOp(operation, value1, value2) {
+    if (operation === "+") {
+        return value1 + value2;
+    } else if (operation === "-") {
+        return value1 - value2;
+    } else if (operation === "*") {
+        return value1 * value2;
+    } else {
+        return value1 / value2;
+    }
+}
+
+console.log(basicOp("+", 5, 10));
+console.log(basicOp("-", 10, 5));
+console.log(basicOp("*", 5, 10));
+console.log(basicOp("/", 10, 5));
