52668e3baf827f7a7c4a2ff2cbbe96b73e98fa60
reggyshicky <reginahshikanda@gmail.com>
2023-10-30 18:31:43 +0300

    extract a string without the first and the last letter using slice(start, end)

---
+++ Frontend/scripts/slice.js
@@ -0,0 +1,10 @@
+//create a function that removes the first and last characters of a string
+//you do not have to worory with strings with less than two characters
+//Syntax: Syntax str.slice(start, end)
+//slice() method returns selected elemetns in an array, as a new array. The slice() method selects from a given start, upto a (not inclusive) given end
+//the slice() method does nit change the original array
+function removeChar(str) {
+    let newstr = str.slice(1, str.length - 1);
+    return newstr
+}
+console.log(removeChar("Reginah"));
